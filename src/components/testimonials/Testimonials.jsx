10468b97a6b97a8fcd90f5e1f6ce73fd1ed8066d
unknown <adeleyepaul2@gmail.com>
2023-07-23 01:47:01 +0100

    deploy error fixed

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -55,7 +55,7 @@ const Testimonials = () => {
             return (
               <SwiperSlide key={index} className='testimonial'>
                 <div className="client__avatar">
-                  <img src={image}/>
+                  <img src={image} alt="testimonial_gap"/>
                 </div>
                 <h5 className='client__name'>{name}</h5>
                   <small className='client__review'>{review}</small>
